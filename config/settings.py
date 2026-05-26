ec05f5bcfe539a80697e6da197a05f8bdadfafef
mickaell <mickaelmoranvera03@gmail.com>
2026-05-26 18:37:56 -0400

    add default pagination to REST_FRAMEWORK (PAGE_SIZE=50)

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,8 @@ REST_FRAMEWORK = {
         'user': '1000/day',
         'chat': '30/hour',
     },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50,
 }
 
 # JWT
